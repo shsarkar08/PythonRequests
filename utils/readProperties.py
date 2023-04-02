e85241ebf98d2d15177c84d4f0ac9b2fae305208
shsarkar08 <sarkar.shah013@gmail.com>
2023-04-02 23:58:55 +0530

    Modified config path

--- utils/readProperties.py
+++ utils/readProperties.py
@@ -1,14 +1,18 @@
+import os
+from pathlib import Path
 import configparser
 
+path = Path(__file__)
+ROOT_DIR = path.parent.parent.absolute()
+config_path = os.path.join(ROOT_DIR, "configurations\\config.ini")
+# config_path = '..\\configurations\\config.ini'
 config = configparser.RawConfigParser()
-config.read('.\\configurations\\config.ini')
+config.read(config_path)
 
 
 class ReadConfig:
 
     @staticmethod
     def getBaseURI():
-        base_uri = config.get('common info', 'BASE_URI')
+        base_uri: str = config.get('common info', 'BASE_URI')
         return base_uri
-
-
